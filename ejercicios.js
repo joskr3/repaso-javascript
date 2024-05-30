7ef30a476ab79db8bdb6aaa6f866f9d6ad9c6f4a
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2024-05-29 20:18:33 -0500

    resuelto segunda  manera

--- ejercicios.js
+++ ejercicios.js
@@ -4,38 +4,38 @@
 // si un numero es multiplo de 3 imprime fizz y si el multiplo de 5 imprime buzz , y si es multiplo de 3 y 5 , imprime fizzbuzz
 
 
-// vamos a prrobar los siguientes numeros : 3,5,15,20,22
+// vamos a probar los siguientes numeros : 3,5,15,20,22
 
 // vamos a crear una funcion que reciba como parametro un numero y devuelva si es fizz(3) , buzz(5) y fizzbuzz si es (3y5)
 
 
 function fizzbuzz( numero = 0 ) { 
-
-
-  if ( ( numero % 3 === 0 ) && ( numero % 5 === 0 ) ) {
-    return 'fizzbuzz'
-  }
   //primer caso -> 3
   // si el numero es multiplo de 3 , entonces la funcion debera devolver(return) 'fizz'
   // una condicion es basicamente -> si....entonces....
   // if(aca dentro va la condicion que sera verdadera(true) o sera falsa(false)) {aca escribo el codigo que necesito}
+  // necesito asegurarme que solamente el numero sera divisible por 3 , y no por 5 y 3 al mismo tiempo
+
+  let noDivisiblePorTresYCincoAlMismoTiempo = !( ( numero % 3 === 0 ) && ( numero % 5 === 0 )  ) 
 
 
-
-  if (numero % 3 === 0) {
+  if (numero % 3 === 0 && noDivisiblePorTresYCincoAlMismoTiempo) {
       return 'fizz'
    }
 
   //segundo caso -> 5
   // si el numero es multiplo de 5 , entonces la funcion debera devolver(return) 'buzz'
-  if ( numero % 5 === 0 ) {
+  if ( numero % 5 === 0 && noDivisiblePorTresYCincoAlMismoTiempo ) {
     return 'buzz'
   }
 
   //tercer caso -> 15
   // si el numero es multiplo de 5 , entonces la funcion debera devolver(return) 'fizzbuzz'
   
-  
+
+  if ( ( numero % 3 === 0 ) && ( numero % 5 === 0 ) ) {
+    return 'fizzbuzz'
+  }
 
 }
 
